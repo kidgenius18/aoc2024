23efdba38a04c0cc7b83a77936668765a97b205e
beaker612 <brian.kerley@gmail.com>
2024-12-01 05:47:48 -0600

    day1 solution

--- 01/day01.py
+++ 01/day01.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os,sys, time, datetime
+from collections import Counter, defaultdict
 sys.path.extend(['..', '.'])
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,30 +11,53 @@ from utils import *
 def get_day(): return 1
 def get_year(): return 2024
 
-def p1(v):
-    lns = get_lines(v)
-    chunks = v.split('\n\n')
-    ans = 0
+
+def create_lists(lns):
+    lns1, lns2 = [],[]
     for ln in lns:
-        ans += 1
+        col1, col2 = map(int, ln.split())
+        lns1.append(col1)
+        lns2.append(col2)
+    lns1.sort()
+    lns2.sort()
+    return lns1, lns2
+
+def p1(v):
+    t0 = time.time()
+    lns = get_lines(v)
+    lns1, lns2 = create_lists(lns)
+
+    ans = sum(abs(a - b) for a, b in zip(lns1, lns2))
+
     return ans
 
 def p2(v):
-    return p1(v)
+    t0 = time.time()
+    lns = get_lines(v)
+
+    lns1, lns2 = create_lists(lns)
+    
+    first_col = defaultdict(int,Counter(lns1))
+    second_col = defaultdict(int,Counter(lns2))
+
+    ans = sum((value * key * second_col[key]) for key,value in first_col.items())
+
+    print(f'Time: {time.time() - t0}')
+    return ans
 
 
 if __name__ == '__main__':
     cmds = get_commands()
-    """
+    
     cmds = [
         #'print_stats',
         'run1',
         #'submit1',
-        #'run2',
+        'run2',
         #'submit2',
         #'run_samples',
         #'samples_only'
         ]
-    """
-    print('Commands:', cmds)
+    
+    #print('Commands:', cmds)
     main(get_year(), get_day(), p1, p2, cmds, FILE=__file__)
